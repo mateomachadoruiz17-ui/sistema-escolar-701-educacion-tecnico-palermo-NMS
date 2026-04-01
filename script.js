8f490c3f82fd137d7869f05ff125855deb87dcae
mateomachadoruiz17-ui <mateomachadoruiz17@gmail.com>
2026-04-01 14:48:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let users = {
+
+  // 👨‍🎓 ESTUDIANTES (36)
+  dante:{pass:"4821",role:"student"},
+  nelson:{pass:"1934",role:"student"},
+  daniela:{pass:"7402",role:"student"},
+  daniel:{pass:"9183",role:"student"},
+  alejandro:{pass:"5529",role:"student"},
+  sara:{pass:"6671",role:"student"},
+  amaia:{pass:"7740",role:"student"},
+  ani:{pass:"8892",role:"student"},
+  santiago:{pass:"9901",role:"student"},
+  estiven:{pass:"1209",role:"student"},
+  camila:{pass:"3304",role:"student"},
+  valeri:{pass:"4408",role:"student"},
+  maria:{pass:"5512",role:"student"},
+  antonia:{pass:"6623",role:"student"},
+  mariana:{pass:"7735",role:"student"},
+  felipe:{pass:"8846",role:"student"},
+  mateo:{pass:"9957",role:"student"},
+  jeray:{pass:"1018",role:"student"},
+  joel:{pass:"2129",role:"student"},
+  dana:{pass:"3231",role:"student"},
+  miguel:{pass:"4342",role:"student"},
+  sharol:{pass:"5453",role:"student"},
+  leonel:{pass:"6564",role:"student"},
+  celeste:{pass:"7675",role:"student"},
+  james:{pass:"8786",role:"student"},
+  joshua:{pass:"9897",role:"student"},
+  mia:{pass:"1357",role:"student"},
+  julian:{pass:"2468",role:"student"},
+  laura:{pass:"3579",role:"student"},
+  franchesco:{pass:"4680",role:"student"},
+  elian:{pass:"5791",role:"student"},
+  carlos:{pass:"6802",role:"student"},
+  andres:{pass:"7913",role:"student"},
+
+  // 👨‍🏫 ADMINS
+  representante:{pass:"7741",role:"admin"},
+  natalia:{pass:"5592",role:"admin"},
+
+  // 👀 VISUALIZADOR
+  napoleon:{pass:"6638",role:"viewer"}
+
+};
